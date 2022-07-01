19dfd868707cdcbf7e049c88a216020c7f0c874a
upMICSD <up201707400@fe.up.pt>
2022-07-01 01:17:17 +0100

    Several changes to test.js

--- test.js
+++ test.js
@@ -1,21 +1,8 @@
 const VL53L1X = require('./vl53l1x');
 const I2C = require('raspi-i2c').I2C;
-const NUM_OF_DATA = 50;
+const NUM_OF_DATA = 500;
+const NUM_OF_CAL = 50;
 
-var fs = require('fs');
-const csv = require('csvtojson');
-const { Parser } = require('json2csv');
-//const parser = require('csv-parser');
-//import  { stringify } from 'csv-stringify';
-//var stringify = require('csv-stringify');
-
-/*
-async function export_to_csv (array, file) {
-    console.log(array);
-    const data = new Parser({fields: array}).parse(array);
-    fs.writeFileSync(file, data);
-}
-*/
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -23,6 +10,7 @@ function sleep(ms) {
 
 async function main () {
     const sensor = new VL53L1X ({
+        //acrescentar array de sensor addresses
         i2c: new I2C()
     });
 
@@ -38,6 +26,7 @@ async function main () {
     await sensor.sensorInit();
 
     //Distance Mode = LONG
+    await sensor.setDistanceMode(VL53L1X.DISTANCE_MODE_LONG);
     const distanceMode = await sensor.getDistanceMode();
     console.log(`distanceMode = ${distanceMode === VL53L1X.DISTANCE_MODE_SHORT ? 'SHORT' : 'LONG'}`);
 
@@ -48,17 +37,50 @@ async function main () {
     //Inter Measurement Interval
     const iM = await sensor.getInterMeasurementInMs();
     console.log(`inter measurement interval = ${iM}ms`);
+    
+    /*const jsonData = fetch(configs.json); 
+    const file = JSON.parse(jsonData);
+    console.log("tou aqui");
+    console.log(file);*/
 
+    
     try {
         //RefSPAD Calibration
         //??
 
         //Ofset Calibration
+        //normal procedure:
         await sensor.setOffset(await sensor.calibrateOffset(140));
+        //await sensor.setOffset(7.633599999999999);
         //console.log('\nOffset: ' + await sensor.getOffset() + '\n');
+/*
+        //for tests, calibrate offset 50 times and get the medium value for the tests in this situation
+        console.log('\nAvg Distance,Offset');
+        let sum_offset = 0;
+        let sum_dist = 0;
+        for (let index = 0; index < NUM_OF_CAL; index++) {
+            let a = await sensor.calibrateOffset(140);
+            sum_offset += a;
+            //await sensor.setOffset(a);
+            //let b = await sensor.getDistance();
+            //sum_dist += b;
+        }
+        sum_offset = sum_offset/NUM_OF_CAL;
+        console.log(`AVG OFFSET VALUE = ${sum_offset} mm`);
+        //sum_dist = sum_dist/NUM_OF_CAL;
+        //console.log(`Avg distance measured each offset: ${sum_dist} mm`);
+        await sensor.setOffset(sum_offset);
+        console.log('\nOffset: ' + await sensor.getOffset() + '\n');
+*/
 
         //Xtalk Calibration (should only be performed if there is a cover glass)
-        await sensor.setXtalk(await sensor.calibrateXtalk(140));
+        //await sensor.setXtalk(await sensor.calibrateXtalk(140));
+
+
+
+        
+        const d = new Date();
+        console.log(`\n${d}\n`);
 
         //Start Ranging
         await sensor.startRanging();
@@ -66,13 +88,17 @@ async function main () {
         let time_array = [];
         let distance_array = [];
 
+        
         //Long Distance Mode set of data
         const start = new Date();
         let count = 0;
+        let real = 3500; //real distance
+        console.log('Real distance,Distance measured');
         for (let i = 0; i < NUM_OF_DATA; i += 1) {
             await sensor.waitForDataReady();
             const distance = await sensor.getDistance();
             //console.log(`Distance = ${distance} mm`);
+            console.log(`${real},${distance}`);
             count += distance;
             const aux = new Date();
             let timex = aux-start;
@@ -85,22 +111,24 @@ async function main () {
             await sensor.clearInterrupt();
             await sleep(500);
         }
-        console.log(data_array);
-        console.log(`Average measuremt: ${count/NUM_OF_DATA} mm`);
+        //console.log(data_array);
+        console.log(`\nAverage measuremt: ${count/NUM_OF_DATA} mm`);
+        const end = new Date();
+        console.log(`Tempo de medição: ${(end.getTime() - start.getTime())/1000}`);
+
         //export_to_csv(data_array,"data.csv");
-        /*
-        stringify(data_array, { header: true }, function (err, output) {
-            fs.writeFile(__dirname+'/someData.csv', output);
-            console.log('File created successfuly');
-        });
-        */
+        
+        //stringify(data_array, { header: true }, function (err, output) {
+        //    fs.writeFile(__dirname+'/someData.csv', output);
+        //    console.log('File created successfuly');
+        //});
 
         
-        
+        /*
         //Distance Mode = SHORT
         await sensor.setDistanceMode(VL53L1X.DISTANCE_MODE_SHORT);
         const distanceMode2 = await sensor.getDistanceMode();
-        //console.log(`distanceMode = ${distanceMode2 === VL53L1X.DISTANCE_MODE_SHORT ? 'SHORT' : 'LONG'}`);
+        console.log(`distanceMode = ${distanceMode2 === VL53L1X.DISTANCE_MODE_SHORT ? 'SHORT' : 'LONG'}`);
         const timingBudgetInMs2 = await sensor.getTimingBudgetInMs();
         //console.log(`timing budget = ${timingBudgetInMs2}ms`);
 
@@ -108,13 +136,16 @@ async function main () {
         for (let i = 0; i < NUM_OF_DATA; i += 1) {
             await sensor.waitForDataReady();
             const distance = await sensor.getDistance();
-            //console.log(`Distance = ${distance} mm`);
+            console.log(`Distance = ${distance} mm`);
+            const rangeStatus = await sensor.getRangeStatus();
+            console.log(`Range status = ${rangeStatus}`);
             await sensor.clearInterrupt();
             await sleep(500);
         }
 
         const end = new Date();
         console.log(`Rate: ${100000/(end-start)}Hz`);
+        */
 
     } finally {
         await sensor.stopRanging();
